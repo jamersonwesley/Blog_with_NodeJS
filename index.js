7b4c1107c40ea65d507afa22e2c39eb7a90485de
JamersonWesley <jamerson_wesley@hotmail.com>
2022-03-08 14:01:14 -0300

    08/03/2022

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.post('/',(req,res)=>{
+       tasks.push(req.body.task);
+       res.render('index',{tasklist:tasks});
+})
+app.post('/deletar/:id',(req,res)=>{
+    tasks.push(req.body.task);
+    res.render('index',{tasklist:tasks});
+})
+
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public',express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'/views'));
+
+app.get('/',(req,res)=>{
+    if(req.query.busca == null){
+        res.send("Home");
+    }else
+    {res.send("Você buscou  " + req.query.busca)}
+})
+
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug)
+})
+
+app.listen(5000,()=>{
+    console.log('rodando');
+})
